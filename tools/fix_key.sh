aefa97176ea0d7088ae23994f0ef2c56aa6e429c
vibhor kumar srivastava <vibhor1608@gmail.com>
2019-08-03 00:46:37 +0530

    Add files via upload

---
+++ tools/fix_key.sh
@@ -0,0 +1,10 @@
+# original lmdb paths
+Original_Train_LMDB=/data/jiecaoyu/imagenet/lmdb/ilsvrc12_train_lmdb_badkey/
+Original_Val_LMDB=/data/jiecaoyu/imagenet/lmdb/ilsvrc12_val_lmdb_badkey/
+
+# target lmdb paths
+Target_Train_LMDB=/data/jiecaoyu/imagenet/lmdb/ilsvrc12_train_lmdb/
+Target_Val_LMDB=/data/jiecaoyu/imagenet/lmdb/ilsvrc12_val_lmdb/
+
+python convert_key.py --source $Original_Train_LMDB --target $Target_Train_LMDB
+python convert_key.py --source $Original_Val_LMDB --target $Target_Val_LMDB
